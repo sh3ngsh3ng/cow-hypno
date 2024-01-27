efbaac2c50e3ed3e60fa21f82af41b33418a4bf8
ys <ysdeveloper01@gmail.com>
2024-01-28 02:49:51 +0800

    added services page

--- src/views/ServicesView.vue
+++ src/views/ServicesView.vue
@@ -4,12 +4,90 @@
 
 <template>
   <div id="services-page-div">
-  <h1>Services Page</h1>
+    <section>
+      <p class="section-header">We offer free consultations</p>
+      <img alt="..." />
+      <p class="p1">
+        The purpose of a free hypnotherapy consultation is to allow potential clients to discuss their needs, goals, and
+        concerns with the hypnotherapist. It helps evaluate the client's readiness for therapy session and address any
+        inquiries. The free pre-consultation builds trust and ensures both parties are comfortable before starting
+        therapy session.
+      </p>
+      <button class="standard-btn">Book your free consultation now!</button>
+
+    </section>
+    <section>
+      <p class="section-header">Informed Consent</p>
+      <img alt="..." />
+      <p class="p1">
+        Hypnosis is a powerful technique that allows you to open the doorway to your subconscious mind with your full
+        consent by utilizing your focused attention and relaxation. In this relaxed state, you become more receptive to
+        positive suggestions and can use your imagination to manifest positive change more effectively.
+      </p>
+      <p class="p1">
+        Although generally safe, hypnosis should only be conducted by a licensed and trained professional. Rest assured
+        that a competent hypnotherapist should be able to help you manage any negative experiences or feelings that may
+        arise during the hypnosis therapy session with ease.
+      </p>
+      <button class="standard-btn">View form here</button>
+    </section>
+    <section>
+      <div id="card-div">
+        <!-- Hypnosis Therapy Card -->
+        <div class="service-card">
+          <img alt="..." />
+          <p class="t1">Hypnosis Therapy</p>
+          <p class="p1">
+            Hypnosis Therapy is the best and effective therapy to clear its trauma and promote mental health and also
+            able to help you overcome any psychological barriers or self-limiting beliefs that may be inhibiting their
+            ability to fully engage in lifestyle activities.
+          </p>
+          <button class="service-btn standard-btn">Read more here</button>
+        </div>
+        <!-- HypnoArt Therapy Card -->
+        <div class="service-card">
+          <img alt="..." />
+          <p class="t1">HypnoArt Therapy</p>
+          <p class="p1">
+            By combining hypnotherapy and art, individuals can gain insight into their thoughts and emotions and find
+            new ways to cope with challenges. This approach can have positive effects on mental health and well-being.
+          </p>
+          <button class="service-btn standard-btn">Read more here</button>
+        </div>
+        <!-- HypnoDance Therapy Card -->
+        <div class="service-card">
+          <img alt="..." />
+          <p class="t1">HypnoDance Therapy</p>
+          <p class="p1">
+            Combining hypnosis and fusion dance offers a transformative experience, enhancing mind-body connection,
+            well-being, and personal growth. Working with skilled practitioners ensures safe and effective application
+            for healing, self-expression, and teamwork.</p>
+          <button class="service-btn standard-btn">Read more here</button>
+        </div>
+        <!-- HypnoWorkout Therapy Card -->
+        <div class="service-card">
+          <img alt="..." />
+          <p class="t1">HypnoWorkout Therapy</p>
+          <p class="p1">
+            The Mindfulness Workout combines hypnosis and exercise to enhance the mind-body connection, reduce stress,
+            and improve overall well-being through present-moment awareness, relaxation, and mental clarity.</p>
+          <button class="service-btn standard-btn">Read more here</button>
+        </div>
+        <!-- Health Coach Card -->
+        <div class="service-card">
+          <img alt="..." />
+          <p class="t1">Health Coach</p>
+          <p class="p1">
+            A health coach helps individuals achieve their health goals through personalized plans, guidance, and
+            accountability. They focus on nutrition, exercise, stress management, sleep, and behavior change to improve
+            overall well-being.</p>
+          <button class="service-btn standard-btn">Read more here</button>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <style scoped>
-#services-page-div {
-
-}
+  #services-page-div {}
 </style>
