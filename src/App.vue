69cd33ff973de0624faf65c1424ee3b63729511e
sh3ngsh3ng <ysdeveloper01@gmail.com>
2024-01-27 23:14:13 +0800

    basic setup with bootstrap and router

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
+  <main>
+    <div id="router-div">
+      <RouterView />
+    </div>
+    <button class="btn btn-primary">Testing</button>
+  </main>
+</template>
+
+<style scoped></style>
