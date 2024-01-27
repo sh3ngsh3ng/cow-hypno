9c4583ddf139ebcde73060f3ac135402e778c4fb
ys <ysdeveloper01@gmail.com>
2024-01-28 03:02:53 +0800

    added skeleton of contact page

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -4,12 +4,50 @@
 
 <template>
   <div id="contact-page-div">
-  <h1>Contact Page</h1>
+    <section>
+      <div id="contact-main-div">
+        <div class="contact-div">
+          <p class="section-header">Social Media</p>
+          <div class="socials-logo-div">
+            insert logos here
+          </div>
+          <p class="p1">Facebook: C O W Soulmate</p>
+          <p class="p1">Tiktok: @c.o.w.soulmate</p>
+          <p class="p1">Instagram: c.o.w.soulmate</p>
+        </div>
+        <div class="contact-div">
+          <p class="section-header">Contact details</p>
+          <p class="p1">+65 9109 9629</p>
+          <p class="p1">support@cowsoulmate.sg</p>
+        </div>
+        <div class="contact-div">
+          <p class="section-header">Opening Hours</p>
+          <p class="p1">Mobile locations/online based on booking</p>
+          <p class="p1">Weekdays: 09:00 - 22:00</p>
+          <p class="p1">Saturday: 09:30 - 23:30</p>
+          <p class="p1">Sunday: 10:00 - 16:00</p>
+        </div>
+      </div>
+    </section>
+
+    <section>
+      <img alt="??" />
+      <p class="section-header">Your feedback is valuable to us</p>
+      <p class="p1">If you have any feedback regarding any of your experiences with C.O.W. Soulmate please use our
+        feedback form. Your feedback will allow us to improve our service and give you a better experience next time!
+      </p>
+      <button class="standard-btn">Feedback form</button>
+    </section>
+
   </div>
 </template>
 
 <style scoped>
-#contact-page-div {
+  .contact-div {
+    padding: 20px;
+  }
 
-}
+  .contact-div>p {
+    text-align: center;
+  }
 </style>
