ae8145d73c279478af10a09185130ad8ab9ccf52
ys <ysdeveloper01@gmail.com>
2024-04-05 04:27:58 +0800

    added hypnoworkout

--- src/views/HypnoWorkout.vue
+++ src/views/HypnoWorkout.vue
@@ -1,13 +1,19 @@
 <script>
+    import brief from '@/assets/svg/hypnosis-view/brief.svg';
     import hypnoworkout from '@/assets/svg/hypnoworkout-view/hypnoworkout.svg';
+    import hypnoworkoutlg from '@/assets/svg/hypnoworkout-view/hypnoworkout-lg.svg';
     import offer from '@/assets/svg/hypnoworkout-view/offer.svg';
+    import offerlg from '@/assets/svg/hypnoworkout-view/offer-lg.svg';
     import consultation from '@/assets/svg/base/consultation.svg';
     export default {
         data() {
             return {
                 hypnoworkout,
+                hypnoworkoutlg,
                 offer,
-                consultation
+                offerlg,
+                consultation,
+                brief
             }
         }
     }
@@ -31,27 +37,57 @@
             </div>
 
             <section class="hypnosis-section">
+                <div class="desktop-view">
+                    <img :src="brief" class="hypnosis-image">
+                </div>
+                <div class="hypnosis-section-left">
+                    <p class="section-header">The brief on HypnoWorkout</p>
+                    <div class="mobile-view">
+                        <img :src="brief" class="hypnosis-image">
+                    </div>
+                    <p class="p1">
+                        HypnoWorkout is an innovative and effective approach to improving mental and physical
+                        well-being. The integration of both hypnosis & exercise enhances self-awareness, reduces stress
+                        and anxiety, improves physical mobility, and promotes emotional release. By addressing both
+                        mental and physical aspects, this combined approach facilitates overall healing and well.
+                    </p>
+                    <p class="p1">
+                        Although generally safe, it's important to note that HypnoWorkout should only be conducted by
+                        qualified professionals who are trained in both hypnotherapy and personal trainer techniques.
+                        Each session is tailored to the individual needs of the client, and outcomes may vary depending
+                        on the client's goals and experiences.
+                    </p>
+                </div>
+            </section>
+
+            <section class="hypnosis-section">
                 <div class="hypnosis-section-right">
                     <p class="section-header">What is HypnoWorkout?</p>
                     <div class="mobile-view">
                         <img :src="hypnoworkout" class="hypnosis-image">
                     </div>
                     <p class="p1">
-                        Hypnotherapy is a powerful type of therapy that enables you to overcome unwanted habits and
-                        behaviors by accessing your subconscious mind. This is where habits and behaviors take root and
-                        persist, making it challenging to alter them independently.
+                        HypnoWorkout is a therapy that combines hypnosis and exercise works by addressing both the mind
+                        and body for holistic healing.
                     </p>
                     <p class="p1">
-                        Remember that hypnotherapy should not replace medical treatment and should always be used
-                        together with other treatments as advised by a licensed medical professional. While hypnotherapy
-                        can be a useful tool for managing your mental health, it is not a one-size-fits-all solution.
-                        Each person’s situation is unique and a professional hypnotherapist can help determine if
-                        hypnotherapy is the right approach for you.
+                        A trained professional guides the individual through relaxation techniques and verbal
+                        suggestions to help them reach a state of deep relaxation and heightened awareness. After the
+                        hypnosis session, individuals engage in a workout session designed to help them release
+                        endorphins, which can reduce pain and increase happiness.
+                    </p>
+                    <p class="p1">
+                        This session also reduces stress levels by decreasing cortisol and increasing serotonin and
+                        dopamine.
+                        It allows individuals to tap into their physical energy and release any stress or tension that
+                        may have been holding them back, while improving self-esteem and confidence through setting and
+                        achieving goals.
+                        It can even improve sleep and provide structure and purpose in one's life.
                     </p>
                 </div>
 
                 <div class="desktop-view">
-                    <img :src="hypnoworkout" class="hypnosis-image">
+                    <img :src="hypnoworkoutlg" class="hypnosis-image max-h-img">
                 </div>
             </section>
 
@@ -62,21 +98,34 @@
                         <img :src="offer" class="hypnosis-image">
                     </div>
                     <p class="p1">
-                        Hypnotherapy is a powerful type of therapy that enables you to overcome unwanted habits and
-                        behaviors by accessing your subconscious mind. This is where habits and behaviors take root and
-                        persist, making it challenging to alter them independently.
+                        HypnoWorkout combines hypnosis and exercise is a powerful type of therapy that allows you to
+                        quiet your mind and let go of any negative thoughts or emotions that may be weighing you down.
+                        This process is an essential step towards achieving a more positive outlook on life, which can
+                        have a significant impact on your overall well-being.
                     </p>
                     <p class="p1">
-                        Remember that hypnotherapy should not replace medical treatment and should always be used
-                        together with other treatments as advised by a licensed medical professional. While hypnotherapy
-                        can be a useful tool for managing your mental health, it is not a one-size-fits-all solution.
-                        Each person’s situation is unique and a professional hypnotherapist can help determine if
-                        hypnotherapy is the right approach for you.
+                        The workout session that follows is designed to help you release any pent-up stress and tension,
+                        leaving you feeling refreshed and energized.
+                    </p>
+                    <p class="p1">
+                        HypnoWorkout is more than just a workout routine, it is body, spirit, and emotions are all
+                        connected and related. Trauma, stress, and other negative experiences can cause tension and
+                        other physiological changes in the body, leading to physical discomfort and illness and it helps
+                        you recognize these physical responses to emotional experiences and works to release the tension
+                        and promote overall well-being.
+                    </p>
+                    <p class="p1">
+                        At HypnoWorkout, we pride ourselves on providing a safe and supportive environment for
+                        individuals to explore their physical and emotional selves.
+                    </p>
+                    <p class="p1">
+                        This part of the program is crucial for anyone looking to improve their body posture, physical
+                        health and fitness levels.
                     </p>
                 </div>
 
                 <div class="desktop-view">
-                    <img :src="offer" class="hypnosis-image">
+                    <img :src="offerlg" class="hypnosis-image max-h-img">
                 </div>
             </section>
 
