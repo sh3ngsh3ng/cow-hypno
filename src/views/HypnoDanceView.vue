6582c65d793c2caa4b56762a41504b31c70e524b
ys <ysdeveloper01@gmail.com>
2024-04-05 04:21:40 +0800

    added hypnodance

--- src/views/HypnoDanceView.vue
+++ src/views/HypnoDanceView.vue
@@ -1,13 +1,19 @@
 <script>
+    import brief from '@/assets/svg/hypnosis-view/brief.svg';
     import hypnodance from '@/assets/svg/hypnodance-view/hypnodance.svg';
+    import hypnodancelg from '@/assets/svg/hypnodance-view/hypnodance-lg.svg';
     import offer from '@/assets/svg/hypnodance-view/offer.svg';
+    import offerlg from '@/assets/svg/hypnodance-view/offer-lg.svg';
     import consultation from '@/assets/svg/base/consultation.svg';
     export default {
         data() {
             return {
                 hypnodance,
+                hypnodancelg,
                 offer,
-                consultation
+                offerlg,
+                consultation,
+                brief
             }
         }
     }
@@ -29,6 +35,28 @@
                 <div>></div>
                 <div class="selected-therapy-tab" @click="$sharedMethods.navigateTo('/hypnodance')">HypnoDance</div>
             </div>
+            <section class="hypnosis-section">
+                <div class="desktop-view">
+                    <img :src="brief" class="hypnosis-image">
+                </div>
+                <div class="hypnosis-section-left">
+                    <p class="section-header">The brief on HypnoDance</p>
+                    <div class="mobile-view">
+                        <img :src="brief" class="hypnosis-image">
+                    </div>
+                    <p class="p1">
+                        HypnoDance is a unique workout routine that goes beyond physical exercise. It combines guided
+                        hypnotic inductions and movement suggestions to help individuals achieve balance, clarity, and
+                        empowerment, tapping into the deep resources of an individuals mind and body connection.
+                    </p>
+                    <p class="p1">
+                        Although generally safe, it's important to note that HypnoDance should only be conducted by
+                        qualified professionals who are trained in both hypnotherapy and dance therapy techniques. Each
+                        session is tailored to the individual needs of the client, and outcomes may vary depending on
+                        the client's goals and experiences.
+                    </p>
+                </div>
+            </section>
 
             <section class="hypnosis-section">
                 <div class="hypnosis-section-right">
@@ -37,21 +65,28 @@
                         <img :src="hypnodance" class="hypnosis-image">
                     </div>
                     <p class="p1">
-                        Hypnotherapy is a powerful type of therapy that enables you to overcome unwanted habits and
-                        behaviors by accessing your subconscious mind. This is where habits and behaviors take root and
-                        persist, making it challenging to alter them independently.
+                        HypnoDance is a treatment that combines hypnosis with dance to help individuals let go of
+                        negative thoughts and feelings and gain confidence and positivity.
                     </p>
                     <p class="p1">
-                        Remember that hypnotherapy should not replace medical treatment and should always be used
-                        together with other treatments as advised by a licensed medical professional. While hypnotherapy
-                        can be a useful tool for managing your mental health, it is not a one-size-fits-all solution.
-                        Each person’s situation is unique and a professional hypnotherapist can help determine if
-                        hypnotherapy is the right approach for you.
+                        Here's how HypnoDance can transform their life:
+                    <ol>
+                        <li>Relaxation and Stress Reduction: By syncing movement with hypnotic suggestions, individuals
+                            can enter a state of deep relaxation, releasing tension and promoting a sense of calm.</li>
+                        <li>Enhanced Self-Expression: Dance allows for authentic self-expression, while hypnosis
+                            facilitates tapping into subconscious emotions and thoughts.</li>
+                        <li>Cognitive Enhancement: Engaging in dance and hypnosis simultaneously stimulates various
+                            brain regions, leading to improved cognitive function. Attention, memory, and
+                            problem-solving abilities can be enhanced through the integration of these practices.</li>
+                        <li>Emotional Healing and Personal Growth: By exploring movement and engaging the subconscious
+                            mind, individuals can uncover and address emotional blocks, promoting personal
+                            transformation.</li>
+                    </ol>
                     </p>
                 </div>
 
                 <div class="desktop-view">
-                    <img :src="hypnodance" class="hypnosis-image">
+                    <img :src="hypnodancelg" class="hypnosis-image max-h-img">
                 </div>
             </section>
 
@@ -62,21 +97,32 @@
                         <img :src="offer" class="hypnosis-image">
                     </div>
                     <p class="p1">
-                        Hypnotherapy is a powerful type of therapy that enables you to overcome unwanted habits and
-                        behaviors by accessing your subconscious mind. This is where habits and behaviors take root and
-                        persist, making it challenging to alter them independently.
+                        HypnoDance merges hypnosis and dance for therapy, leveraging the mind-body connection for
+                        relaxation, healing, and personal growth. Utilizing dance and hypnosis, it taps into your inner
+                        resources to boost overall well-being.
                     </p>
                     <p class="p1">
-                        Remember that hypnotherapy should not replace medical treatment and should always be used
-                        together with other treatments as advised by a licensed medical professional. While hypnotherapy
-                        can be a useful tool for managing your mental health, it is not a one-size-fits-all solution.
-                        Each person’s situation is unique and a professional hypnotherapist can help determine if
-                        hypnotherapy is the right approach for you.
+                        The treatment involves a highly effective hypnosis session, followed by a liberating dance
+                        session where you can freely express themselves through movement and relish the moment.
+                    </p>
+                    <p class="p1">
+                        Each session typically lasts between 1.5 to 2 hours, which includes the time for inducing
+                        hypnosis.
+                    </p>
+                    <p class="p1">
+                        Join our HypnoDance , you can:
+                    <ul>
+                        <li>Transform your life through movement and hypnosis with HypnoDance!</li>
+                        <li>Discover the power of mind-body connection with HypnoDance!</li>
+                        <li>Release stress, express yourself, and find inner harmony with HypnoDance!</li>
+                        <li>Experience the fusion of dance and hypnosis for holistic well-being with HypnoDance!</li>
+                        <li>Unlock your true potential and embrace personal growth with HypnoDance!</li>
+                    </ul>
                     </p>
                 </div>
 
                 <div class="desktop-view">
-                    <img :src="offer" class="hypnosis-image">
+                    <img :src="offerlg" class="hypnosis-image max-h-img">
                 </div>
             </section>
 
